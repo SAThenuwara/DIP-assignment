34d6d9f0bbce6c96b5912149de1553491dbc3191
Dasun9518 <it21009518@my.sliit.lk>
2023-10-28 13:27:00 +0530

    Filter update 1

--- ui2.py
+++ ui2.py
@@ -191,10 +191,6 @@ class App(customtkinter.CTk):
 
         #configure advanced tab
 
-
-                #configure advanced tab
-
-
         #sharpening
 
         #checkbox sharpening
@@ -203,9 +199,9 @@ class App(customtkinter.CTk):
         self.checkbox_advanced_sharp.grid(row=0, column=0, padx=5, pady=(20, 0))
 
         #slider sharpening
-        self.slider_advanced_sharp = customtkinter.CTkSlider(self.tabview.tab("Advanced"), from_=0, to=100, command=self.slider_advanced_sharp_event)
-        self.slider_advanced_sharp.configure(number_of_steps=100)
-        self.slider_advanced_sharp.grid(row=1, column=0, padx=5, pady=(20, 0))
+        #self.slider_advanced_sharp = customtkinter.CTkSlider(self.tabview.tab("Advanced"), from_=-1, to=0, command=self.slider_advanced_sharp_event)
+        #self.slider_advanced_sharp.configure(number_of_steps=100)
+        #self.slider_advanced_sharp.grid(row=1, column=0, padx=5, pady=(20, 0))
 
 
         #smoothing
@@ -213,12 +209,12 @@ class App(customtkinter.CTk):
         #checkbox smoothing
         self.check_var_advanced_smooth = customtkinter.StringVar(value="off")
         self.checkbox_advanced_smooth = customtkinter.CTkCheckBox(self.tabview.tab("Advanced"), text="   Smoothing", command=self.checkbox_advanced_smooth_event, variable=self.check_var_advanced_smooth, onvalue="on", offvalue="off")
-        self.checkbox_advanced_smooth.grid(row=3, column=0, padx=5, pady=(20, 0))
+        self.checkbox_advanced_smooth.grid(row=2, column=0, padx=5, pady=(20, 0))
 
         #slider smoothing 
         self.slider_advanced_smooth = customtkinter.CTkSlider(self.tabview.tab("Advanced"), from_=0, to=100, command=self.slider_advanced_smooth_event)
         self.slider_advanced_smooth.configure(number_of_steps=100)
-        self.slider_advanced_smooth.grid(row=4, column=0, padx=5, pady=(20, 0))
+        self.slider_advanced_smooth.grid(row=3, column=0, padx=5, pady=(20, 0))
 
 
         #filter 3
@@ -226,12 +222,12 @@ class App(customtkinter.CTk):
         #checkbox filter 3
         self.check_var_advanced_filter3 = customtkinter.StringVar(value="off")
         self.checkbox_advanced_filter3 = customtkinter.CTkCheckBox(self.tabview.tab("Advanced"), text="   Filter 3", command=self.checkbox_advanced_filter3_event, variable=self.check_var_advanced_filter3, onvalue="on", offvalue="off")
-        self.checkbox_advanced_filter3.grid(row=5, column=0, padx=5, pady=(20, 0))
+        self.checkbox_advanced_filter3.grid(row=4, column=0, padx=5, pady=(20, 0))
 
         #slider filter 3
         self.slider_advanced_filter3 = customtkinter.CTkSlider(self.tabview.tab("Advanced"), from_=0, to=100, command=self.slider_advanced_filter3_event)
         self.slider_advanced_filter3.configure(number_of_steps=100)
-        self.slider_advanced_filter3.grid(row=6, column=0, padx=5, pady=(20, 0))
+        self.slider_advanced_filter3.grid(row=5, column=0, padx=5, pady=(20, 0))
 
 
         #filter 4
@@ -239,67 +235,40 @@ class App(customtkinter.CTk):
         #checkbox filter 4
         self.check_var_advanced_filter4 = customtkinter.StringVar(value="off")
         self.checkbox_advanced_filter4 = customtkinter.CTkCheckBox(self.tabview.tab("Advanced"), text="   Filter 4", command=self.checkbox_advanced_filter4_event, variable=self.check_var_advanced_filter4, onvalue="on", offvalue="off")
-        self.checkbox_advanced_filter4.grid(row=7, column=0, padx=5, pady=(20, 0))
+        self.checkbox_advanced_filter4.grid(row=6, column=0, padx=5, pady=(20, 0))
 
         #slider filter 4
         self.slider_advanced_filter4 = customtkinter.CTkSlider(self.tabview.tab("Advanced"), from_=0, to=100, command=self.slider_advanced_filter4_event)
         self.slider_advanced_filter4.configure(number_of_steps=100)
-        self.slider_advanced_filter4.grid(row=8, column=0, padx=5, pady=(20, 0))
+        self.slider_advanced_filter4.grid(row=7, column=0, padx=5, pady=(20, 0))
 
         #threshold button
         self.button_thresh = customtkinter.CTkButton(master=self.tabview.tab("Advanced"), fg_color="transparent", text="Threshold", border_width=2, text_color=("gray10", "#DCE4EE"),
                                                                command=self.button_thresh_event)
-        self.button_thresh.grid(row=9, column=0, padx=(40, 40), pady=(40, 10), sticky="nsew")
+        self.button_thresh.grid(row=8, column=0, padx=(40, 40), pady=(40, 10), sticky="nsew")
 
         #edge detect button
         self.button_edge_detect = customtkinter.CTkButton(master=self.tabview.tab("Advanced"), fg_color="transparent", text="Edge Detect", border_width=2, text_color=("gray10", "#DCE4EE"),
                                                                command=self.button_edge_detect_event)
-        self.button_edge_detect.grid(row=10, column=0, padx=(40, 40), pady=(10, 10), sticky="nsew")
+        self.button_edge_detect.grid(row=9, column=0, padx=(40, 40), pady=(10, 10), sticky="nsew")
 
         #tonal transform
         self.button_tonal_transform = customtkinter.CTkButton(master=self.tabview.tab("Advanced"), fg_color="transparent", text="Tonal Transform", border_width=2, text_color=("gray10", "#DCE4EE"),
                                                                command=self.button_tonal_transform_event)
-        self.button_tonal_transform.grid(row=11, column=0, padx=(40, 40), pady=(10, 0), sticky="nsew")
+        self.button_tonal_transform.grid(row=10, column=0, padx=(40, 40), pady=(10, 0), sticky="nsew")
 
         #tonal input box
         self.inputBox_tonal_transform_alpha = customtkinter.CTkEntry(self.tabview.tab("Advanced"), placeholder_text="Enter alpha")
-        self.inputBox_tonal_transform_alpha.grid(row=12, column=0, padx=(40, 40), pady=(5, 0), sticky="nsew")
+        self.inputBox_tonal_transform_alpha.grid(row=11, column=0, padx=(40, 40), pady=(5, 0), sticky="nsew")
 
         self.inputBox_tonal_transform_beta = customtkinter.CTkEntry(self.tabview.tab("Advanced"), placeholder_text="Enter beta")
-        self.inputBox_tonal_transform_beta.grid(row=13, column=0, padx=(40, 40), pady=(5, 0), sticky="nsew")
-
-        #old code
-
-        #Sharpening title
-        #self.title_advanced_filterType = customtkinter.CTkLabel(self.tabview.tab("Advanced"), text="Sharpening", anchor="w")
-        #self.title_advanced_filterType.grid(row=0, column=0, padx=20, pady=(10, 0))
-
-        #filter list
-        #self.optionmenu_advanced_filterType = customtkinter.CTkOptionMenu(self.tabview.tab("Advanced"), dynamic_resizing=False, values=["Type 1", "Type 2", "Type 3", "Type 4"],
-                                                                          #command=self.advanced_filterType_option) #filter type menu function)
-        #self.optionmenu_advanced_filterType.grid(row=1, column=0, padx=20, pady=(5,10))
-
-        #filter value input box
-        #self.inputBox_advanced_filterValue = customtkinter.CTkEntry(self.tabview.tab("Advanced"), placeholder_text="Enter Filter Value")
-        #self.inputBox_advanced_filterValue.grid(row=2, column=0, padx=(30, 30), pady=(0, 10), sticky="nsew")
-
-        #filter apply button
-        #self.main_button_advanced_filterApply = customtkinter.CTkButton(master=self.tabview.tab("Advanced"), fg_color="transparent", text="Apply Now", border_width=2, text_color=("gray10", "#DCE4EE"),
-                                                                        #command=self.button_advanced_filterApply_event)
-        #self.main_button_advanced_filterApply.grid(row=3, column=0, padx=(40, 40), pady=(0, 0), sticky="nsew")
-
-        #intensity manipulation title
-        #self.title_advanced_intensityManipulation = customtkinter.CTkLabel(self.tabview.tab("Advanced"), text="Intensity Manipulation", anchor="w")
-        #self.title_advanced_intensityManipulation.grid(row=4, column=0, padx=20, pady=(20, 0))
-
-
-
+        self.inputBox_tonal_transform_beta.grid(row=12, column=0, padx=(40, 40), pady=(5, 0), sticky="nsew")
 
 
         #reset button
         self.main_button_basic_reset = customtkinter.CTkButton(master=self.tabview.tab("Advanced"), fg_color="transparent", text="Reset Values", border_width=2, text_color=("gray10", "#DCE4EE"),
                                                                command=self.button_basic_reset_event)
-        self.main_button_basic_reset.grid(row=14, column=0, padx=(80, 10), pady=(50, 0), sticky="nsew")
+        self.main_button_basic_reset.grid(row=13, column=0, padx=(80, 10), pady=(50, 0), sticky="nsew")
 
 
 
@@ -330,22 +299,6 @@ class App(customtkinter.CTk):
         # set default values
         
         self.appearance_mode_optionemenu.set("System")
-        #self.sidebar_button_3.configure(state="disabled", text="Disabled CTkButton")
-        #self.checkbox_3.configure(state="disabled")
-        #self.checkbox_1.select()
-        #self.scrollable_frame_switches[0].select()
-        #self.scrollable_frame_switches[4].select()
-        #self.radio_button_3.configure(state="disabled")
-        #self.scaling_optionemenu.set("100%")
-        #self.optionmenu_1.set("CTk Option menu")
-        #self.combobox_1.set("CTkComboBox")
-        #self.slider_1.configure(command=self.progressbar_2.set)
-        #self.slider_2.configure(command=self.progressbar_3.set)
-        #self.progressbar_1.configure(mode="indeterminnate")
-        #self.progressbar_1.start()
-        #self.textbox.insert("0.0", "CTkTextbox\n\n" + "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.\n\n" * 20)
-        #self.seg_button_1.configure(values=["CTkSegmentedButton", "Value 2", "Value 3"])
-        #self.seg_button_1.set("Value 2")
 
 
 #defining functions
@@ -492,6 +445,8 @@ class App(customtkinter.CTk):
                         tk.messagebox.showerror("Error", "Invalid input. Please enter valid width and height.")    
             elif crop_state == "off":
                 tk.messagebox.showerror("Error", "Turn on cropping.")  
+
+
     #flip x button function
     def button_basic_flip_x_event(self):
         if hasattr(self, 'image'):
@@ -512,6 +467,7 @@ class App(customtkinter.CTk):
             self.image_output.configure(image=self.tk_image_out)
             print("flipping y")
 
+
     #reset button function
     def button_basic_reset_event(self):
         if hasattr(self, 'image'):
@@ -531,10 +487,46 @@ class App(customtkinter.CTk):
 
     #sharpening checkbox event
     def checkbox_advanced_sharp_event(self):
+        
+        sharp_state = self.check_var_advanced_sharp.get()
+        if hasattr(self, 'image'):
+            if sharp_state == "on":
+                kernel = np.array([[0, -1, 0],
+                            [-1, 5, -1],
+                            [0, -1, 0]])
+                self.sharpened_image = cv2.cvtColor(self.image_out, cv2.COLOR_BGR2RGB)
+                self.sharpened_image = cv2.filter2D(self.sharpened_image, -1, kernel)
+                self.sharp_image = cv2.resize(self.sharpened_image, (400, 400))
+                self.sharp_image= cv2.cvtColor(self.sharp_image, cv2.COLOR_BGR2RGB)
+                img_pil_out = Image.fromarray(self.sharp_image)
+                self.tk_image_out = ImageTk.PhotoImage(image=img_pil_out)
+                self.image_output.configure(image=self.tk_image_out) 
+
+            elif sharp_state == "off":
+                self.rgb_image = cv2.resize(self.image2, (400, 400))
+                self.rgb_image= cv2.cvtColor(self.rgb_image, cv2.COLOR_BGR2RGB)
+                img_pil_out = Image.fromarray(self.rgb_image)
+                self.tk_image_out = ImageTk.PhotoImage(image=img_pil_out)
+                self.image_output.configure(image=self.tk_image_out)     
+        
         print("sharpeness checkbox toggled, current value:", self.check_var_advanced_sharp.get())
 
     #sharpening slider event
     def slider_advanced_sharp_event(self, value):
+        
+        val = int(value)
+        kernel = np.array([[0, -1, 0],
+                            [-1, 5, -1],
+                            [0, -1, 0]])
+
+        self.sharpened_image = cv2.cvtColor(self.image_out, cv2.COLOR_BGR2RGB)
+        self.sharpened_image = cv2.filter2D(self.sharpened_image, -1, kernel)
+        self.sharp_image = cv2.resize(self.sharpened_image, (400, 400))
+        self.sharp_image= cv2.cvtColor(self.sharp_image, cv2.COLOR_BGR2RGB)
+        img_pil_out = Image.fromarray(self.sharp_image)
+        self.tk_image_out = ImageTk.PhotoImage(image=img_pil_out)
+        self.image_output.configure(image=self.tk_image_out) 
+
         print("sharpness is: ", value)
 
 
@@ -542,6 +534,25 @@ class App(customtkinter.CTk):
 
     #smoothness checkbox event
     def checkbox_advanced_smooth_event(self):
+        smooth_state = self.check_var_advanced_smooth.get()
+        if hasattr(self, 'image'):
+            if smooth_state == "on":
+                self.smoothed_image = cv2.cvtColor(self.image_out, cv2.COLOR_BGR2RGB)
+                self.smoothed_image = cv2.GaussianBlur(self.smoothed_image, (5, 5), 0)
+                self.smooth_image = cv2.resize(self.smoothed_image, (400, 400))
+                self.smooth_image= cv2.cvtColor(self.smooth_image, cv2.COLOR_BGR2RGB)
+                img_pil_out = Image.fromarray(self.smooth_image)
+                self.tk_image_out = ImageTk.PhotoImage(image=img_pil_out)
+                self.image_output.configure(image=self.tk_image_out) 
+
+            elif smooth_state == "off":
+                self.rgb_image = cv2.resize(self.image2, (400, 400))
+                self.rgb_image= cv2.cvtColor(self.rgb_image, cv2.COLOR_BGR2RGB)
+                img_pil_out = Image.fromarray(self.rgb_image)
+                self.tk_image_out = ImageTk.PhotoImage(image=img_pil_out)
+                self.image_output.configure(image=self.tk_image_out)    
+
+        #self.blur_image = cv2.GaussianBlur(self.image_out, (15, 15), 0)
         print("smoothness checkbox toggled, current value:", self.check_var_advanced_smooth.get())
 
     #smoothness slider event
